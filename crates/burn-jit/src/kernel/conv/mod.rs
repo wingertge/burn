fbd7aa3888237df53dc3548e55ec0fcab728e739
Genna Wingert <wingertge@gmail.com>
2024-09-17 17:54:21 +0200

    Merge

--- crates/burn-jit/src/kernel/conv/mod.rs
+++ crates/burn-jit/src/kernel/conv/mod.rs
@@ -7,5 +7,7 @@ mod deform_conv_transpose2d;
 pub(crate) use conv2d::*;
 pub(crate) use conv3d::*;
 pub(crate) use conv_transpose3d::*;
+pub(crate) use deform_conv2d::*;
+pub(crate) use deform_conv_transpose2d::*;
 
 pub use conv2d::{conv2d, conv_transpose2d, Conv2dStrategy, ConvTranspose2dStrategy};
